3297d8e17b4af8cce15ac46db036bb2a1f7a5104
Monica Grevious <mag.amtrust@gmail.com>
2023-12-29 02:17:36 -0500

    Added typography and color scheme to tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import theme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#014E56",
+        secondary: "#F67E7E",
+        "rapture-blue": "#79C8C7",
+        "police-blue": "#2C6269",
+        "deep-jungle-green": "#004047",
+        "sacramento-state-green": "#012F34",
+        "dark-green": "#002529",
+      },
+      fontFamily: {
+        sans: ['"Livvic"', ...theme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
