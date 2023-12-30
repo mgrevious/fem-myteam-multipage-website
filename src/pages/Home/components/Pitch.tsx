6027665682bfa515c7dd803dbf9b4091962959e7
Monica Grevious <mag.amtrust@gmail.com>
2023-12-29 22:21:49 -0500

    Added styling for tablet and desktop. Desktop is still in progress.

--- src/pages/Home/components/Pitch.tsx
+++ src/pages/Home/components/Pitch.tsx
@@ -8,10 +8,16 @@ interface Props {
 
 const Pitch: React.FC<Props> = ({ icon, header, content }) => {
   return (
-    <section className="mt-14 flex flex-col items-center">
-      <img src={icon.src} alt={icon.alt} className="mb-6" />
-      <h3 className="text-secondary text-center font-bold mb-3">{header}</h3>
-      <p className="text-center">{content}</p>
+    <section className="mt-14 sm:mt-10 flex flex-col sm:flex-row items-center sm:justify-center">
+      <img src={icon.src} alt={icon.alt} className="mb-6 sm:mb-0 sm:mr-7" />
+      <div className="sm:mt-6">
+        <h3 className="text-secondary text-center sm:text-left font-bold mb-3 sm:mb-5 text-lg">
+          {header}
+        </h3>
+        <p className="text-center sm:text-left opacity-60 w-full leading-6">
+          {content}
+        </p>
+      </div>
     </section>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,29 +17,42 @@ import './home.scss';
 const Home = () => {
   return (
     <>
-      <div className="bg-home-section-1 py-10 px-6">
+      <div className="bg-home-section-1 py-10 px-6 sm:px-10 lg:px-32">
         <header className="text-lg text-center font-semibold text-secondary">
           <nav>
-            <div className="flex justify-between">
-              <img src={logo} alt="MyTeam Logo" className="w-32" />
-              <button>
+            <div className="flex justify-between items-center">
+              <div className="flex justify-start">
+                {' '}
+                <img src={logo} alt="MyTeam Logo" className="w-32 sm:mr-12" />
+                <div className="text-white hidden sm:flex sm:gap-10">
+                  <a href="">home</a>
+                  <a href="">about</a>
+                </div>
+              </div>
+              <button className="hidden sm:inline-block h-12 font-semibold text-lg border-2 text-white border-white rounded-full px-8">
+                contact us
+              </button>
+              <button type="button" className="sm:hidden">
                 <img src={hamburgerMenu} alt="" />
               </button>
             </div>
           </nav>
         </header>
-        <section className="mt-24">
-          <h1 className="text-4xl text-center font-bold px-16 mb-6">
+        <section className="mt-24 sm:mt-28 lg:flex lg:justify-between lg:items-end lg:mb-24">
+          <h1 className="text-4xl sm:text-[64px] sm:leading-[64px] text-center font-bold mb-6 px-16 lg:p-0 sm:px-40 lg:text-left lg:text-[100px] lg:leading-[1em] lg:m-0 lg:w-[65%] lg:pt-4">
             Find the best <span className="text-secondary">talent</span>
           </h1>
-          <p className="text-center">
-            Finding the right people and building high performing teams can be
-            hard. Most companies aren’t tapping into the abundance of global
-            talent. We’re about to change that.
-          </p>
+          <div className="lg:w-[35%] lg:flex lg:flex-col lg:justify-between">
+            <div className="hidden lg:block lg:w-[50px] h-1 bg-rapture-blue mb-24"></div>
+            <p className="text-center leading-7 mb-40 sm:px-32 lg:text-left lg:text-lg lg:m-0 lg:px-0 lg:pb-1">
+              Finding the right people and building high performing teams can be
+              hard. Most companies aren’t tapping into the abundance of global
+              talent. We’re about to change that.
+            </p>
+          </div>
         </section>
       </div>
-      <div className="bg-sacramento-state-green bg-home-section-2 py-10 px-6">
+      <div className="bg-sacramento-state-green bg-home-section-2 py-16 px-5 sm:p-24">
         <div className="relative pt-9">
           <div className="w-[50px] bg-secondary h-1 absolute top-0 left-0"></div>
           <h2 className="text-[32px] font-bold mb-6 w-2/3">
@@ -63,8 +76,8 @@ const Home = () => {
           />
         </div>
       </div>
-      <div className="bg-home-section-3 px-6 testimonials">
-        <h2 className="text-[32px] text-center font-bold pt-36 mb-4">
+      <div className="bg-home-section-3 py-36 testimonials sm:pt-28 sm:px-28 sm:pb-0">
+        <h2 className="text-[32px] text-center font-bold mb-4">
           Delivering real results for top companies. Some of our{' '}
           <span className="text-rapture-blue">success stories.</span>
         </h2>
@@ -90,39 +103,50 @@ const Home = () => {
           content=" “The team perfectly fit the specialized skill set required. They focused on the most essential features helping us launch the platform eight months faster than planned.”"
         />
       </div>
-      <div className="bg-secondary flex justify-center items-center px-10 py-20">
-        <section className="flex flex-col justify-center">
-          <h3 className="text-sacramento-state-green text-3xl font-bold mb-7 text-center">
+      <div className="bg-secondary flex justify-center items-center px-10 py-20 sm:px-24 bg-home-section-4">
+        <section className="flex flex-col sm:flex-row justify-center sm:justify-between sm:items-center sm:w-full">
+          <h3 className="text-sacramento-state-green text-3xl font-bold mb-7 sm:mb-0 text-center">
             Ready to get started?
           </h3>
           <div className="text-center">
-            <button className="h-12 font-semibold text-lg text-sacramento-state-green border-2 border-sacramento-state-green rounded-full px-10">
+            <button className="h-12 font-semibold text-lg text-sacramento-state-green border-2 border-sacramento-state-green rounded-full px-8">
               contact us
             </button>
           </div>
         </section>
       </div>
-      <div className="py-20 bg-sacramento-state-green">
+      <div className="py-16 sm:py-16 sm:px-10 bg-dark-green">
         <div className="flex flex-col justify-center items-center">
-          <img src={logo} alt="Footer myteam logo" className="w-[95px] mb-9" />
-          <div className="flex justify-center align-center gap-9">
-            <a href="">home</a>
-            <a href="">about</a>
+          <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center sm:w-full sm:items-start">
+            {' '}
+            <div className="flex flex-col items-center sm:items-start">
+              <img
+                src={logo}
+                alt="Footer myteam logo"
+                className="w-[95px] mb-9"
+              />
+              <div className="flex justify-center align-center gap-9 sm:gap-5">
+                <a href="">home</a>
+                <a href="">about</a>
+              </div>
+            </div>
+            <ul className="list-none text-center sm:text-right my-10 sm:m-0 opacity-60">
+              <li className="mb-2">987 Hillcrest Lane</li>
+              <li className="mb-2">Irvine, CA</li>
+              <li className="mb-2">California 92714</li>
+              <li className="mb-2">Call Us : 949-833-7432</li>
+            </ul>
           </div>
-          <ul className="list-none text-center mt-10 mb-10 opacity-60">
-            <li className="mb-2">987 Hillcrest Lane</li>
-            <li className="mb-2">Irvine, CA</li>
-            <li className="mb-2">California 92714</li>
-            <li className="mb-2">Call Us : 949-833-7432</li>
-          </ul>
-          <div className="flex gap-4 mb-5">
-            <img src={facebookIcon} alt="Facebook Icon" />
-            <img src={pinterestIcon} alt="Pinterest Icon" />
-            <img src={twitterIcon} alt="Twitter Icon" />
+          <div className="sm:flex sm:flex-row sm:justify-between sm:w-full sm:mt-10">
+            <div className="flex justify-center gap-4 mb-5">
+              <img src={facebookIcon} alt="Facebook Icon" />
+              <img src={pinterestIcon} alt="Pinterest Icon" />
+              <img src={twitterIcon} alt="Twitter Icon" />
+            </div>
+            <p className="text-center opacity-60">
+              Copyright 2020. All Rights Reserved
+            </p>
           </div>
-          <p className="text-center opacity-60">
-            Copyright 2020. All Rights Reserved
-          </p>
         </div>
       </div>
     </>
