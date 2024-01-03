32933e14460575aab39c3c976282e39872a51926
redd1120 <monica.grevious@gmail.com>
2024-01-03 14:08:40 -0500

    Added homepage, predeploy and deploy commands. removed console.log

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  console.log('mode: ', mode);
   return defineConfig({
     base: mode === 'development' ? '/' : '/fem-myteam-multi-page-website/',
     plugins: [react()],
