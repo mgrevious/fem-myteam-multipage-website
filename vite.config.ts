c35c9f313de8ab12ec479db077d63e13333fd494
redd1120 <monica.grevious@gmail.com>
2024-01-03 14:12:50 -0500

    Updated the homepage and base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
-    base: mode === 'development' ? '/' : '/fem-myteam-multi-page-website/',
+    base: mode === 'development' ? '/' : '/fem-myteam-multipage-website/',
     plugins: [react()],
   });
 };
